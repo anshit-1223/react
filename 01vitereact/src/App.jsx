7ef6f58f4b9dc9cf0b4e5ca32527a692aae4ac9d
Anshit Verma <anshit@kali>
2025-04-12 23:15:01 +0530

    Create New Component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Chai from "./chai"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Learn React with Anshit </h1>
+      <Chai />
     </>
   )
 }
