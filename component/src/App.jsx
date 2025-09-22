c93e5abe8b5b9a2d1dc885cca4d6099c7e7b5378
anshit-1223 <anshit1223@gmail.com>
2025-09-22 22:29:14 +0530

    Add Component

---
+++ component/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header'
+import Footer from './Footer'
+function App(){
+  return (
+  <>
+  <Header />
+  <h1>Body Component</h1>
+  <Footer />
+  </>
+  );
+}
+
+export default App
---
+++ component/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <>
+        <h1>This is Footer Component</h1>
+        </>
+    );
+}
+export default Footer
